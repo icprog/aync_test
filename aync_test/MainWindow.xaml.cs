22d00666787573e98c485253732e8353e680deec
Wang Yu <wangyu60@foxmail.com>
2018-04-02 22:50:08 +0800

    tt

--- aync_test/MainWindow.xaml.cs
+++ aync_test/MainWindow.xaml.cs
@@ -1,18 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace aync_test
 {
@@ -28,7 +16,7 @@ namespace aync_test
 
         private void Button_OnClick(object sender, RoutedEventArgs e)
         {
-            this.spinnerWait.Visibility = System.Windows.Visibility.Visible;
+            this.spinnerWait.Visibility = Visibility.Visible;
             this.spinnerWait.Spin = true;
             this.lblStatus.Content = "Thread is running";
             Task t = Task.Factory.StartNew(
@@ -37,7 +25,7 @@ namespace aync_test
             t.ContinueWith(antecednet =>
             {
                 this.lblStatus.Content = "Thread not running.";
-                this.spinnerWait.Visibility = System.Windows.Visibility.Hidden;
+                this.spinnerWait.Visibility = Visibility.Hidden;
                 this.spinnerWait.Spin = false;
             }, TaskScheduler.FromCurrentSynchronizationContext());
         }
