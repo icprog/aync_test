c5dab47afab607bd26152b48c42fa20e736f5e02
wangy <wangy@SGPN12303>
2018-04-09 13:48:05 +0800

    add sample code using await

--- aync_test/MainWindow.xaml.cs
+++ aync_test/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Net.Http;
+using System.Windows;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -9,6 +11,7 @@ namespace aync_test
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
@@ -18,16 +21,80 @@ namespace aync_test
         {
             this.spinnerWait.Visibility = Visibility.Visible;
             this.spinnerWait.Spin = true;
+            this.button1.IsEnabled = false;
+            this.button2.IsEnabled = false;
             this.lblStatus.Content = "Thread is running";
             Task t = Task.Factory.StartNew(
-                () => Thread.Sleep(5000)
-            );
-            t.ContinueWith(antecednet =>
+                () =>
+                {
+                    Thread.Sleep(5000);
+                }
+            ).ContinueWith(antecednet =>
             {
-                this.lblStatus.Content = "Thread not running.";
+                this.lblStatus.Content = "Thread stopped.";
                 this.spinnerWait.Visibility = Visibility.Hidden;
                 this.spinnerWait.Spin = false;
+                this.button1.IsEnabled = true;
+                this.button2.IsEnabled = true;
             }, TaskScheduler.FromCurrentSynchronizationContext());
         }
+
+        private async void Button1_OnClick(object sender, RoutedEventArgs e)
+        {
+            DateTime time = await GetStopTime();
+            this.lblStatus.Content = $"Task stopped at time {time.ToLocalTime()}";
+
+            this.spinnerWait.Spin = false;
+            this.spinnerWait.Visibility = Visibility.Collapsed;
+            this.button.IsEnabled = true;
+            this.button2.IsEnabled = true;
+        }
+
+        private async void Button2_OnClick(object sender, RoutedEventArgs e)
+        {
+            var length = await GetMsdnStringLth();
+            this.lblStatus.Content = $"Task finished. MSDN page character length is {length}";
+
+            this.spinnerWait.Spin = false;
+            this.spinnerWait.Visibility = Visibility.Collapsed;
+            this.button.IsEnabled = true;
+            this.button1.IsEnabled = true;
+        }
+
+        private async Task<DateTime> GetStopTime()
+        {
+
+            Task<DateTime> task = new Task<DateTime>(() =>
+            {
+                //Task.Delay(5000);
+                Thread.Sleep(5000);
+                return DateTime.Now;
+            });
+
+            this.lblStatus.Content = "Waiting for async task to finish...";
+            this.spinnerWait.Spin = true;
+            this.spinnerWait.Visibility = Visibility.Visible;
+            this.button.IsEnabled = false;
+            this.button2.IsEnabled = false;
+
+            task.Start();
+            DateTime time = await task.ConfigureAwait(false);
+            return time;
+        }
+
+        private async Task<int> GetMsdnStringLth()
+        {
+            this.lblStatus.Content = "Waiting for async task to finish...";
+            this.spinnerWait.Spin = true;
+            this.spinnerWait.Visibility = Visibility.Visible;
+            this.button.IsEnabled = false;
+            this.button1.IsEnabled = false;
+
+            Task<string> getStringTask = new HttpClient().GetStringAsync("http://msdn.microsoft.com");
+            string urlContents = await getStringTask;
+            return urlContents.Length;
+        }
+
+     
     }
 }
---
+++ msdnExample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace msdnExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Length > 1)
+            {
+                Console.WriteLine("program start..");
+                GetPageSizeAsync(args[1]).Wait();
+                Console.WriteLine("after async call..");
+            }
+            else
+                Console.WriteLine("Enter at least one URL on the command line.");
+        }
+        private static async Task GetPageSizeAsync(string url)
+        {
+            var client = new HttpClient();
+            var uri = new Uri(Uri.EscapeUriString(url));
+            byte[] urlContents = await client.GetByteArrayAsync(uri);
+            Console.WriteLine($"{url}: {urlContents.Length / 2:N0} characters");
+        }
+        // The following call from the command line:
+        //    await1 http://docs.microsoft.com
+        // the program displays output like the following: 
+        //   http://docs.microsoft.com: 7,967 characters
+    }
+}
